4c92055fe235b856e2ce81a0e01f34c63148d94e
Lucas C <luccoelhoo@gmail.com>
2020-07-17 16:41:30 -0300

    Quinto upload de dados. Inclui o script para agendamento no crontab.

---
+++ crontab-py.py
@@ -0,0 +1,15 @@
+from crontab import CronTab
+from pathlib import Path
+
+filename='Video-all-regions-num.py'
+cron = CronTab(user=True)
+job = cron.new(command=str(str("cd ")+ str(Path.cwd())+str(" && ")+str('/usr/bin/python3 '+ str(Path.cwd())+'/'+filename)))
+job.hour.every(20)
+cron.write()
+
+
+filename='Video-all-regions-obitos.py'
+cron = CronTab(user=True)
+job = cron.new(command=str(str("cd ")+ str(Path.cwd())+str(" && ")+str('/usr/bin/python3 '+ str(Path.cwd())+'/'+filename)))
+job.hour.every(21)
+cron.write()
